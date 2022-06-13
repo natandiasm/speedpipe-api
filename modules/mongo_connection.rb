0a448d697493bd5eeb3c3268275672997d5fbb3a
Natan Dias <natand@speediomail.com>
2022-06-13 20:19:47 -0300

    Procfile

--- modules/mongo_connection.rb
+++ modules/mongo_connection.rb
@@ -1,5 +1,5 @@
 module Mongo_connection
-    @con = Mongo::Client.new(["95.217.227.107:27017"], :database => "external_contribution_miami", user: "#{ENV["DB_LOGIN"]}", password: "#{ENV["DB_PASSWD"]}", auth_source: "#{ENV["auth_source"]}", auth_mech: :scram)
+    @con = Mongo::Client.new([""], :database => "", user: "#{ENV[""]}", password: "#{ENV[""]}", auth_source: "#{ENV[""]}", auth_mech: :scram)
 
     def self.con
         @con
