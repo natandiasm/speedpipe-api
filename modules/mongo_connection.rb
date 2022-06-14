22467783b646b0a71319f909fb5d0f975646db34
Natan Dias <natand@speediomail.com>
2022-06-14 13:16:47 -0300

    teste mongo

--- modules/mongo_connection.rb
+++ modules/mongo_connection.rb
@@ -1,8 +1,6 @@
 module Mongo_connection
     @uri = Mongo::URI.new(ENV['MONGO_URL'])
-    @client = Mongo::Client.new(@uri.servers, @uri.options)
-    @client.login(@uri.credentials)
-    @client[@uri.database]
+    @client = Mongo::Client.new(@uri.servers, @uri.options, @uri.credentials, @uri.database)
     def self.cli
         @client
     end
